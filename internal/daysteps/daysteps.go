fd4e785015e757ed98b4ada2ccf7944ec8f8e554
Гульшат Сагдатуллина <gulshat.sagdatullina@mediascope.net>
2025-07-13 12:47:42 +0300

    first iteration

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,14 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -11,10 +18,61 @@ const (
 	mInKm = 1000
 )
 
+// parsePackage возвращает количество шагов, продолжительность прогулки и ошибку в случае её возникновения
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+
+	if len(data) == 0 {
+		return 0, 0, errors.New("отсутствуют данные о прогулке")
+	}
+
+	parts := strings.Split(data, ",")
+	if len(parts) != 2 {
+		return 0, 0, errors.New("некорректные данные о прогулке")
+	}
+
+	stepsTmp := parts[0]
+	durationTmp := parts[1]
+
+	steps, err := strconv.Atoi(stepsTmp)
+	if err != nil || steps <= 0 {
+		return 0, 0, errors.New("некорректные данные о количестве пройденных шагов")
+	}
+
+	duration, err := time.ParseDuration(durationTmp)
+	if err != nil || duration <= 0 {
+		return 0, 0, errors.New("некорректные данные о продолжительности прогулки")
+	}
+
+	return steps, duration, nil
+
 }
 
+// DayActionInfo возвращает количество шагов, дистанцию в километрах и количество потраченных калорий
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+
+	if len(data) == 0 {
+		log.Println("отсутствуют данные о прогулке")
+		return ""
+	}
+
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	if weight <= 0 || height <= 0 {
+		log.Println("вес и рост должны быть больше 0")
+		return ""
+	}
+
+	path := (float64(steps) * stepLength) / mInKm
+	cals, err := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, path, cals)
+
 }
--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -1,6 +1,11 @@
 package spentcalories
 
 import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -13,26 +18,128 @@ const (
 	walkingCaloriesCoefficient = 0.5  // коэффициент для расчета калорий при ходьбе
 )
 
+// parseTraining возвращает количество шагов, вид активности, её продолжительность и ошибку в случае её возникновения
 func parseTraining(data string) (int, string, time.Duration, error) {
-	// TODO: реализовать функцию
+
+	if len(data) == 0 {
+		return 0, "", 0, errors.New("отсутствуют данные о прогулке")
+	}
+
+	parts := strings.Split(data, ",")
+	if len(parts) != 3 {
+		return 0, "", 0, errors.New("некорректные данные о прогулке")
+	}
+
+	stepsTmp := parts[0]
+	steps, err := strconv.Atoi(stepsTmp)
+	if err != nil || steps <= 0 {
+		return 0, "", 0, errors.New("некорректные данные о количестве пройденных шагов")
+	}
+
+	activity := parts[1]
+
+	durationTmp := parts[2]
+	duration, err := time.ParseDuration(durationTmp)
+	if err != nil || duration <= 0 {
+		return 0, "", 0, errors.New("некорректные данные о продолжительности прогулки")
+	}
+
+	return steps, activity, duration, nil
+
 }
 
+// distance возвращает дистанцию в километрах
 func distance(steps int, height float64) float64 {
-	// TODO: реализовать функцию
+
+	lengthPace := height * stepLengthCoefficient
+	distance := (lengthPace * float64(steps)) / mInKm
+	return distance
 }
 
+// meanSpeed возвращает среднюю скорость
 func meanSpeed(steps int, height float64, duration time.Duration) float64 {
-	// TODO: реализовать функцию
+
+	if duration <= 0 {
+		return 0
+	}
+	pathLength := distance(steps, height)
+	meanSpeed := pathLength / duration.Hours()
+	return meanSpeed
 }
 
+// TrainingInfo возвращает строку с информацией о тренировке.
+//
+// Пример возвращаемой строки:
+//
+//	Тип тренировки: Бег
+//	Длительность: 0.75 ч.
+//	Дистанция: 10.00 км.
+//	Скорость: 13.34 км/ч
+//	Сожгли калорий: 18621.75
 func TrainingInfo(data string, weight, height float64) (string, error) {
-	// TODO: реализовать функцию
+
+	steps, activity, duration, err := parseTraining(data)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	var calories float64
+
+	switch activity {
+	case "Бег":
+		calories, err = RunningSpentCalories(steps, weight, height, duration)
+		if err != nil {
+			log.Println(err)
+			return "", err
+		}
+	case "Ходьба":
+		calories, err = WalkingSpentCalories(steps, weight, height, duration)
+		if err != nil {
+			log.Println(err)
+			return "", err
+		}
+	default:
+		return "", errors.New("неизвестный тип тренировки")
+	}
+
+	distanceKm := distance(steps, height)
+	speed := meanSpeed(steps, height, duration)
+
+	result := fmt.Sprintf(
+		"Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n",
+		activity,
+		duration.Hours(),
+		distanceKm,
+		speed,
+		calories,
+	)
+
+	return result, nil
+
 }
 
+// RunningSpentCalories возвращает количество калорий, потраченных при беге, и ошибку в случае её возникновения
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, errors.New("некорректные данные")
+	}
+	speed := meanSpeed(steps, height, duration)
+	minutes := duration.Minutes()
+	calories := (weight * speed * minutes) / minInH
+	return calories, nil
 }
 
+// WalkingSpentCalories возвращает количество калорий, потраченных при ходьбе, и ошибку в случае её возникновения
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, errors.New("некорректные данные")
+	}
+	speed := meanSpeed(steps, height, duration)
+	minutes := duration.Minutes()
+	calories := (weight * speed * minutes) / minInH
+	calories *= walkingCaloriesCoefficient
+	return calories, nil
+
 }
